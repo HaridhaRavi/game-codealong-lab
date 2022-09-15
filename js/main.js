ea8509c7b5a4ccfabe69176d8c4f80a73bccef45
HaridhaRavi <haridha.gr@gmail.com>
2022-09-15 17:14:03 +0200

    create multiple obstacle

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 console.log("my js file loaded")
-class Game{
+class Game {
     constructor(){
-        this.player = null;
+        this.player = null; //will store an instance of the class Player
+        this.obstacles = [];//store instances of obstacle class
     }
+
     start(){
-        console.log("Starting game");
         this.player = new Player();
         this.attachEventListeners();
+        
+        setInterval(() => {
+            this.obstacles.forEach(obstacleInstance => obstacleInstance.moveDown());    
+        }, 100);
+
+        setInterval(()=>{
+            const newObstacle = new Obstacle();
+            this.obstacles.push(newObstacle);
+        },900)
     }
     attachEventListeners(){
         document.addEventListener('keydown',(keyEvent)=>{
@@ -26,7 +36,7 @@ class Player{
         this.positionX =35;
         this.positionY =0;
         this.width = 20;
-        this.height = 10;
+        this.height = 5;
         this.domElement = null;
         this.createDomElement();
     }
@@ -60,6 +70,43 @@ class Player{
     }
 
 }
+
+
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 90;
+        this.width = 10;
+        this.height = 5;
+        this.domElement = null;
+        this.createDomElement();
+    }
+     //Creating a div using dom and append it in dom
+     createDomElement(){
+        this.domElement = document.createElement('div');
+        //newDiv.setAttribute('id','player')
+        this.domElement.className = "obstacle";
+
+        this.domElement.style.width = this.width+"vw";
+        this.domElement.style.height = this.height+"vh";
+
+        //position
+        this.domElement.style.bottom = this.positionY+"vh";
+        this.domElement.style.left = this.positionX+"vw";
+        
+        //append to dom
+        const boardElm = document.getElementById('board');
+        boardElm.appendChild(this.domElement);
+
+    }
+    moveDown(){
+        console.log("moving down",this.positionY)
+        this.positionY-= 5;
+        this.domElement.style.bottom = this.positionY + "vh";
+    }
+   
+}
+
 
 const game = new Game();
 game.start();
